12bb23bce86407fe306b6f1203956e710f7024f8
christoph.albrecht@charin.global <christoph.albrecht@charin.global>
2023-09-08 19:54:04 +0200

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNzMxNg/spectral.js?branch_slug=main&token=5f0a3c23-8f1e-4c95-b29f-e2203408aa3f";
+export default { extends: ruleset };
